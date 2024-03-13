34d8596de2073b43a2911f8db67515f78a9da990
Fernando Da Silva <fernando.da-silva@ecole-hexagone.com>
2024-03-13 16:40:12 +0100

    remove comments

--- Rendu/database/encryption.cpp
+++ Rendu/database/encryption.cpp
@@ -12,7 +12,6 @@
 
 using namespace std;
 
-
 Password::Password(string &password, bool is_encrypted):
     _raw_value(password) {
         if (is_encrypted) {_encrypted_value = password;}
@@ -21,26 +20,18 @@ Password::Password(string &password, bool is_encrypted):
 
 void Password::encrypt(const string &password) {
     string encrypted_password;
-    // for (char c: password) {
-    //     encrypted_password.append(c + "");
-    // }
-    
 
     const std::string SECRET = "SECRET";
     
-    // Data to hash
     std::string data = password + SECRET;
 
-    // Buffer to store the hash (SHA-256 is 256 bits, i.e., 32 bytes)
     unsigned char hash[SHA256_DIGEST_LENGTH];
 
-    // Initialize a SHA-256 context
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, data.c_str(), data.size());
     SHA256_Final(hash, &sha256);
 
-    // Convert the hash to a hex string
     std::stringstream hexstream;
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         hexstream << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
